485dd1eb22a0b8cd65d4411a3095a939a4eda459
Cubepaas HOST <idc@cubepaas.com>
2020-10-17 06:15:19 +0000

    Added vimplug installation

--- SCRIPTS/vim_deploy.sh
+++ SCRIPTS/vim_deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 sudo apt install vim neovim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 wget -O ~/.vimrc --no-check-certificate https://raw.githubusercontent.com/shuairuan/.config/main/NVIM/init.vim
 mkdir .config
 cd .config
